0025866ba97f56bbf225227caebb388ff88681b1
DIA012 <gudwls0525@naver.com>
2017-11-16 23:10:31 +0900

    텀프로젝트
    메인메뉴 화면

--- app/src/main/java/com/example/kbiid/termproject/MainActivity.java
+++ app/src/main/java/com/example/kbiid/termproject/MainActivity.java
@@ -1,28 +1,60 @@
 package com.example.kbiid.termproject;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-    private Button btn;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth firebaseAuth;
+    private TextView textViewUserEmail;
+    private Button buttonStart;
+    private Button buttonExplain;
+    private Button buttonLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = (Button)findViewById(R.id.startButton);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),
-                                            GamePlayActivity.class);
-                startActivity(intent);
-            }
-        });
+        textViewUserEmail = (TextView) findViewById(R.id.textviewUserEmail);
+        buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonExplain = (Button) findViewById(R.id.buttonExplain);
+        buttonLogout = (Button) findViewById(R.id.buttonLogout);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        if(firebaseAuth.getCurrentUser() == null) {
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        textViewUserEmail.setText("리듬게임");
+        buttonStart.setOnClickListener(this);
+        buttonExplain.setOnClickListener(this);
+        buttonLogout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == buttonStart) {
+            Toast.makeText(this, "게임시작", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this, GamePlayActivity.class));
+        }
+        if(view == buttonExplain) {
+            Toast.makeText(this, "게임방법", Toast.LENGTH_SHORT).show();
+        }
+        if(view == buttonLogout) {
+            firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
     }
 }
